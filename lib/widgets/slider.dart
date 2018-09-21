920a67a06aa10b29dae2920586f08cf4d0083353
Andrew Grosner <abgrosner@gmail.com>
2018-09-21 09:09:35 -0400

    [Slider] add a slider widget.

---
+++ lib/widgets/slider.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'platform.dart';
+
+/// A [PlatformAdaptingWidget] that renders a [Slider] on Android and [CupertinoSlider]
+/// on iOS.
+class PSlider extends PlatformAdaptingWidget {
+  final double value;
+  final ValueChanged<double> onChanged;
+  final ValueChanged<double> onChangeStart;
+  final ValueChanged<double> onChangeEnd;
+  final double min;
+  final double max;
+  final int divisions;
+  final Color activeColor;
+
+  /// See [Slider.activeColor]
+  final Color androidInActiveColor;
+
+  PSlider({
+    @required this.value,
+    @required this.onChanged,
+    this.onChangeEnd,
+    this.onChangeStart,
+    this.max,
+    this.min,
+    this.divisions,
+    this.activeColor,
+    this.androidInActiveColor,
+  });
+
+  @override
+  get renderMaterial => () {
+        return Slider(
+          value: value,
+          onChanged: onChanged,
+          onChangeEnd: onChangeEnd,
+          onChangeStart: onChangeStart,
+          max: max,
+          min: min,
+          divisions: divisions,
+          activeColor: activeColor,
+          inactiveColor: androidInActiveColor,
+        );
+      };
+
+  @override
+  get renderCupertino => () {
+        return CupertinoSlider(
+          value: value,
+          onChanged: onChanged,
+          onChangeStart: onChangeStart,
+          onChangeEnd: onChangeEnd,
+          min: min,
+          max: max,
+          divisions: divisions,
+          activeColor: activeColor,
+        );
+      };
+}
