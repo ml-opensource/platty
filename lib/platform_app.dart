2112d1f1167bba3bc2284b6f35be5c2c08249b4f
Andrew Grosner <abgrosner@gmail.com>
2018-09-24 17:07:58 -0400

    [Platform] add platformapp widget.

---
+++ lib/platform_app.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:platty/widgets/platform.dart';
+
+/// This class adapts the app theme to the specified platform.
+/// On Android this is [MaterialApp]
+/// On iOS this is [CupertinoApp].
+class PlatformApp extends PlatformAdaptingWidget {
+  final Map<String, WidgetBuilder> routes;
+  final String initialRoute;
+  final RouteFactory onGenerateRoute;
+  final RouteFactory onUnknownRoute;
+  final List<NavigatorObserver> navigatorObservers;
+  final TransitionBuilder builder;
+  final String title;
+  final GenerateAppTitle onGenerateTitle;
+  final Widget home;
+  final Color color;
+  final Locale locale;
+  final LocaleResolutionCallback localeResolutionCallback;
+  final Iterable<LocalizationsDelegate<dynamic>> localizationsDelegates;
+  final Iterable<Locale> supportedLocales;
+  final bool showPerformanceOverlay;
+  final bool checkerboardRasterCacheImages;
+  final bool checkerboardOffscreenLayers;
+  final bool showSemanticsDebugger;
+  final bool debugShowCheckedModeBanner;
+
+  PlatformApp({
+    this.routes = const <String, WidgetBuilder>{},
+    this.initialRoute,
+    this.onGenerateRoute,
+    this.onUnknownRoute,
+    this.navigatorObservers = const [],
+    this.builder,
+    this.title = '',
+    this.onGenerateTitle,
+    this.home,
+    this.color,
+    this.locale,
+    this.localizationsDelegates,
+    this.supportedLocales,
+    this.localeResolutionCallback,
+    this.showPerformanceOverlay = false,
+    this.checkerboardRasterCacheImages = false,
+    this.checkerboardOffscreenLayers = false,
+    this.showSemanticsDebugger = false,
+    this.debugShowCheckedModeBanner = true,
+    TargetPlatform renderPlatform,
+  }) : super(renderPlatform: renderPlatform);
+
+  @override
+  get renderMaterial => () => MaterialApp(
+        routes: routes,
+        initialRoute: initialRoute,
+        onGenerateRoute: onGenerateRoute,
+        onUnknownRoute: onUnknownRoute,
+        navigatorObservers: navigatorObservers,
+        builder: builder,
+        title: title,
+        onGenerateTitle: onGenerateTitle,
+        home: home,
+        color: color,
+        locale: locale,
+        localizationsDelegates: localizationsDelegates,
+        supportedLocales: supportedLocales,
+        localeResolutionCallback: localeResolutionCallback,
+        showPerformanceOverlay: showPerformanceOverlay,
+        checkerboardRasterCacheImages: checkerboardRasterCacheImages,
+        checkerboardOffscreenLayers: checkerboardOffscreenLayers,
+        showSemanticsDebugger: showSemanticsDebugger,
+        debugShowCheckedModeBanner: debugShowCheckedModeBanner,
+      );
+
+  @override
+  get renderCupertino => () => CupertinoApp(
+        routes: routes,
+        initialRoute: initialRoute,
+        onGenerateRoute: onGenerateRoute,
+        onUnknownRoute: onUnknownRoute,
+        navigatorObservers: navigatorObservers,
+        builder: builder,
+        title: title,
+        onGenerateTitle: onGenerateTitle,
+        home: home,
+        color: color,
+        locale: locale,
+        localizationsDelegates: localizationsDelegates,
+        supportedLocales: supportedLocales,
+        localeResolutionCallback: localeResolutionCallback,
+        showPerformanceOverlay: showPerformanceOverlay,
+        checkerboardRasterCacheImages: checkerboardRasterCacheImages,
+        checkerboardOffscreenLayers: checkerboardOffscreenLayers,
+        showSemanticsDebugger: showSemanticsDebugger,
+        debugShowCheckedModeBanner: debugShowCheckedModeBanner,
+      );
+}
